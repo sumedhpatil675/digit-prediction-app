a4b642a227d6cfe98b437a608fc73ff24a1e3e32
sumedh-cogg <sumedh@cognitivegarage.ai>
2023-12-05 13:20:36 +0530

    added api integration

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ai_task
+
+
+docker build -t my-flask-app .
+
+docker run -p 4000:80 my-flask-app
+
+docker exec -it 65e22e3af53c /bin/bash
--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 # app.py
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from tensorflow.keras.models import load_model
 import numpy as np
 from PIL import Image
 import io
+import base64
 
 app = Flask(__name__)
 
@@ -13,6 +14,15 @@ model = load_model('models/mnist_model.h5')
 
 def preprocess_image(file):
     img = Image.open(file).convert('L')  # Convert to grayscale
+    img = img.resize((28, 28))
+    img_array = np.array(img).reshape(28, 28, 1)
+    img_array = img_array / 255.0
+    return img_array
+
+def preprocess_image_from_base64(encoded_image):
+    # Decode base64 and process the image
+    decoded_image = io.BytesIO(base64.b64decode(encoded_image))
+    img = Image.open(decoded_image).convert('L')  # Convert to grayscale
     img = img.resize((28, 28))
     img_array = np.array(img).reshape(28, 28, 1)
     img_array = img_array / 255.0
@@ -44,6 +54,21 @@ def predict():
         predicted_label = np.argmax(predictions[0])
 
         return render_template('index.html', prediction=predicted_label)
+
+@app.route('/api/predict', methods=['POST'])
+def api_predict():
+    if request.method == 'POST':
+        try:
+            # Assume JSON input with base64-encoded image
+            data = request.json
+            image_data = preprocess_image_from_base64(data['image'])
+        except Exception as e:
+            return jsonify({'error': f'Error processing image: {str(e)}'})
+
+        predictions = model.predict(np.array([image_data]))
+        predicted_label = np.argmax(predictions[0])
+
+        return jsonify({'prediction': int(predicted_label)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
