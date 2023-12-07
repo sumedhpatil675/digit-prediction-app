1900f01779c870bc82000b790547361d8c54a893
sumedh-cogg <sumedh@cognitivegarage.ai>
2023-12-07 07:53:31 +0530

    added integration for kubernetes

--- app/app.py
+++ app/app.py
@@ -10,15 +10,13 @@ import mysql.connector
 
 app = Flask(__name__)
 
-# Load the saved model
 model = load_model('models/mnist_model.h5')
 
 
-# Function to establish a database connection
 def get_db_connection():
     try:
         db = mysql.connector.connect(
-            host="mysql-db",
+            host="mysql-service",
             user="root",
             password="password",
             database="prediction_db"
@@ -29,7 +27,6 @@ def get_db_connection():
         print(f"Error: {err}")
         return None
 
-# Function to close the database connection
 def close_db_connection(db, cursor):
     if cursor:
         cursor.close()
@@ -37,7 +34,6 @@ def close_db_connection(db, cursor):
         db.close()
         print("Database connection closed")
 
-# Create a table for storing predictions
 def create_predictions_table(cursor):
     try:
         cursor.execute("CREATE TABLE IF NOT EXISTS predictions (id INT AUTO_INCREMENT PRIMARY KEY, image_path VARCHAR(255), prediction INT)")
@@ -45,7 +41,6 @@ def create_predictions_table(cursor):
     except mysql.connector.Error as err:
         print(f"Error: {err}")
 
-# Function to save a prediction to the database
 def save_to_database(image_path, prediction, db, cursor):
     try:
         prediction = int(prediction)
@@ -59,7 +54,6 @@ def save_to_database(image_path, prediction, db, cursor):
 db = get_db_connection()
 if db is None:
     print("not connected")
-    # return jsonify({'error': 'Unable to connect to the database'})
 else:
     print("connected")
     
@@ -68,16 +62,15 @@ create_predictions_table(cursor)
 
 
 def preprocess_image(file):
-    img = Image.open(file).convert('L')  # Convert to grayscale
+    img = Image.open(file).convert('L') 
     img = img.resize((28, 28))
     img_array = np.array(img).reshape(28, 28, 1)
     img_array = img_array / 255.0
     return img_array
 
 def preprocess_image_from_base64(encoded_image):
-    # Decode base64 and process the image
     decoded_image = io.BytesIO(base64.b64decode(encoded_image))
-    img = Image.open(decoded_image).convert('L')  # Convert to grayscale
+    img = Image.open(decoded_image).convert('L')  
     img = img.resize((28, 28))
     img_array = np.array(img).reshape(28, 28, 1)
     img_array = img_array / 255.0
@@ -87,52 +80,25 @@ def preprocess_image_from_base64(encoded_image):
 def index():
     return render_template('index.html')
 
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     if request.method == 'POST':
-#         # Check if the post request has the file part
-#         if 'file' not in request.files:
-#             return render_template('index.html', error='No file part')
-
-#         file = request.files['file']
-
-#         # If the user does not select a file, submit an empty part without filename
-#         if file.filename == '':
-#             return render_template('index.html', error='No selected file')
-
-#         try:
-#             image_data = preprocess_image(file)
-#         except Exception as e:
-#             return render_template('index.html', error=f'Error processing image: {str(e)}')
-
-#         predictions = model.predict(np.array([image_data]))
-#         predicted_label = np.argmax(predictions[0])
-
-#         return render_template('index.html', prediction=predicted_label)
 
 @app.route('/api/predict', methods=['POST'])
 def api_predict():
 
     
-    if request.method == 'POST':
-        # file = request.files['file']
-
-        # if file.filename == '':
-        #     return render_template('index.html', error='No selected file')
-        
+    if request.method == 'POST':        
         try:
             data = request.json
             image_data = preprocess_image_from_base64(data['image'])
             file_name = data['file_name']
         except Exception as e:
-            # close_db_connection(db, cursor)
+            close_db_connection(db, cursor)
             return jsonify({'error': f'Error processing image: {str(e)}'})
 
         predictions = model.predict(np.array([image_data]))
         predicted_label = np.argmax(predictions[0])
         save_to_database(file_name, predicted_label, db, cursor)
 
-        # close_db_connection(db, cursor)
+        close_db_connection(db, cursor)
 
         return jsonify({'prediction': int(predicted_label)})
 
--- mysql/init.sql
+++ mysql/init.sql
@@ -1,6 +1,2 @@
--- Create a database and user for the Flask app
 CREATE DATABASE IF NOT EXISTS prediction_db;
 
--- Change 'root' to the desired username for your Flask app
--- Change 'localhost' to '%' to allow connections from any host (or specify a specific host)
--- GRANT ALL PRIVILEGES ON prediction_db.* TO 'root'@'%' IDENTIFIED BY 'password';
